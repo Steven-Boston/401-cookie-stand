268632204dcc699018a625e206d7a2ffab76ffa2
Steven_Boston <steven.cole.boston@gmail.com>
2021-09-01 21:22:56 -0700

    fixed file structure and updated README

---
+++ Components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props) {
+  return (
+    <footer>
+      <p>&#169; 2021 Pat's Salmon Cookies Inc     {props.stores.length} Locations World Wide</p> 
+    </footer>
+  )
+}
+
+export default Footer
--- fish-cookies/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>&#169; 2021 Pat's Salmon Cookies Inc</p> 
-    </footer>
-  )
-}
-
-export default Footer
